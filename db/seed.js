3d20070574a427938d7e54f91af244342e3a01e5
Michael Nahum <USGrant63@gmail.com>
2016-11-16 21:57:34 -0500

    fixed humiliating bug

--- db/seed.js
+++ db/seed.js
@@ -3,8 +3,11 @@ var seedData    = require("./seeds.json");
 
 var Garment     = mongoose.model("Garment");
 
+console.log('hey')
 Garment.remove({}).then(function(){
-    Garment.collection.insert(seedDate).then(function(){
+  console.log('girl')
+    Garment.collection.insert(seedData).then(function(){
+      console.log('bye bye bye')
       process.exit();
     });
   });
