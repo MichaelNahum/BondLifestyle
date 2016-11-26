71b62e30dae48df83b323274c2ea5f22228ce478
Michael Nahum <USGrant63@gmail.com>
2016-11-25 23:19:48 -0500

    added config variable

--- db/connection.js
+++ db/connection.js
@@ -8,7 +8,7 @@ var GarmentSchema = new mongoose.Schema(
 );
 
 mongoose.model("Garment", GarmentSchema);
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise; //problem here?
 if(process.env.NODE_ENV == "production"){
   mongoose.connect(process.env.MONGOLAB_URL);
 }else{
